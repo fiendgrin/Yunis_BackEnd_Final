d22783b1d45c42aabee8bb3eff3f7fd859da1958
fiendgrin <yunisah@code.edu.az>
2023-10-01 10:58:09 +0400

    'OurProduct' list and 'TopSeller' list done

--- JuanYunis/JuanYunis/Controllers/HomeController.cs
+++ JuanYunis/JuanYunis/Controllers/HomeController.cs
@@ -19,7 +19,14 @@ namespace JuanYunis.Controllers
         {
             HomeVM homeVMs = new HomeVM
             {
-                Sliders = await _context.Sliders.Where(s => s.IsDeleted == false).ToListAsync()
+                Sliders = await _context.Sliders.Where(s => s.IsDeleted == false).ToListAsync(),
+                TopSellers = await _context.Products
+                .Include(p=>p.ProductImages.Where(pi=>pi.IsDeleted==false))
+                .Where(p=>p.IsDeleted ==false && p.IsTopSeller == true).OrderByDescending(p => p.Id).Take(4).ToListAsync(),
+                OurProducts = await _context.Products
+                .Include(p => p.ProductImages.Where(pi => pi.IsDeleted == false))
+                .Where(p => p.IsDeleted == false && p.IsOurProduct == true).OrderByDescending(p => p.Id).ToListAsync(),
+
             };
             return View(homeVMs);
         }
--- JuanYunis/JuanYunis/ViewModels/HomeVMs/HomeVM.cs
+++ JuanYunis/JuanYunis/ViewModels/HomeVMs/HomeVM.cs
@@ -5,5 +5,7 @@ namespace JuanYunis.ViewModels.HomeVMs
     public class HomeVM
     {
         public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<Product> OurProducts { get; set; }
+        public IEnumerable<Product> TopSellers { get; set; }
     }
 }
