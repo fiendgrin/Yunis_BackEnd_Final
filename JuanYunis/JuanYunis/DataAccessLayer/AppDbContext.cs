2454283b7e3c23d3260b2d21e85e7e0d0a634be3
fiendgrin <yunisah@code.edu.az>
2023-10-05 13:08:28 +0400

    Identity System Added

--- JuanYunis/JuanYunis/DataAccessLayer/AppDbContext.cs
+++ JuanYunis/JuanYunis/DataAccessLayer/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using JuanYunis.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace JuanYunis.DataAccessLayer
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
--- JuanYunis/JuanYunis/Program.cs
+++ JuanYunis/JuanYunis/Program.cs
@@ -1,15 +1,32 @@
 using JuanYunis.DataAccessLayer;
 using JuanYunis.Interfaces;
+using JuanYunis.Models;
 using JuanYunis.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration
     .GetConnectionString("Default")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
 
-builder.Services.AddScoped<ILayoutService,LayoutService>();
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+
+    options.Lockout.AllowedForNewUsers = false;
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<ILayoutService, LayoutService>();
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
@@ -19,6 +36,6 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseSession();
 app.MapControllerRoute("Area", "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
-app.MapControllerRoute("default","{Controller=Home}/{Action=Index}/{id?}");
+app.MapControllerRoute("default", "{Controller=Home}/{Action=Index}/{id?}");
 
 app.Run();
